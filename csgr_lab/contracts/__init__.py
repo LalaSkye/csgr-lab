8f343a7a975ea0345b4fe9bd63f9e4cf5ecf1875
Ricky Jones <ricky.mcjones@gmail.com>
2026-02-20 14:29:41 +0000

    Fix lint: sort imports in contracts/__init__.py (I001)

--- csgr_lab/contracts/__init__.py
+++ csgr_lab/contracts/__init__.py
@@ -1,13 +1,13 @@
 """Contracts module: schema definitions, validation, and type system."""
 
+from csgr_lab.contracts.schema import ClauseSpec, ContractSpec
 from csgr_lab.contracts.types import ContractStatus, SeverityLevel
-from csgr_lab.contracts.schema import ContractSpec, ClauseSpec
 from csgr_lab.contracts.validators import validate_contract
 
 __all__ = [
+    "ClauseSpec",
+    "ContractSpec",
     "ContractStatus",
     "SeverityLevel",
-    "ContractSpec",
-    "ClauseSpec",
     "validate_contract",
 ]
